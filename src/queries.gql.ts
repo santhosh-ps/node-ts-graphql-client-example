2f32b8f3bd7d08afe4b6315d7f235c4944bd4bae
Santhosh PS <pssanthosh13@gmail.com>
2022-07-23 15:07:18 +0530

    remove test queries

--- src/queries.gql.ts
+++ src/queries.gql.ts
@@ -7,17 +7,6 @@ gql`query getProjects {
   }
 }`
 
-gql`query getProjectAndIssues($projectNameSearch: String!) {
-  projects(where:{name:{_ilike: $projectNameSearch}}){
-    id
-    name
-    issues{
-      id
-      name
-    }
-  }
-}`
-
 gql`mutation createProjects($projects: [projects_insert_input!]!) {
   insert_projects(objects: $projects) {
     returning {
@@ -26,19 +15,3 @@ gql`mutation createProjects($projects: [projects_insert_input!]!) {
     }
   }
 }`
-
-gql`mutation multiMutationTest($projects: [projects_insert_input!]!) {
-  insert_projects(objects: $projects) {
-    returning {
-      id
-      name
-    }
-  }
-  update_projects(where: {name: {_ilike: "%test%"}}, _set: {name: "name-test"}) {
-    returning {
-      id
-      name
-    }
-  }
-}
-`

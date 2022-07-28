7827d8e715336733f00f240fc9a222ef87616609
Santhosh PS <pssanthosh13@gmail.com>
2022-07-28 18:22:28 +0530

    add pgCrypto installation first in migration

--- src/hasura/migrations/default/1658559038692_create_table_public_projects/up.sql
+++ src/hasura/migrations/default/1658559038692_create_table_public_projects/up.sql
@@ -1,3 +1,5 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
 CREATE TABLE "public"."projects" ("name" varchar NOT NULL, "id" uuid NOT NULL, "created_at" timestamptz NOT NULL DEFAULT now(), "updated_at" timestamptz NOT NULL DEFAULT now(), PRIMARY KEY ("id") );
 CREATE OR REPLACE FUNCTION "public"."set_current_timestamp_updated_at"()
 RETURNS TRIGGER AS $$
